9a87a84b2d4806876100b3e8e540c284c70a4657
Dmitriy An <pancake0890@mail.ru>
2019-09-25 19:55:59 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function multiply(first, second) {
   // your solution
+  let a = BigInt(first);
+  let b = BigInt(second);
+  let result = (a * b).toString();
+
+  return result;
 }
